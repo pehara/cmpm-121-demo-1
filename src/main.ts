d357eee388a382d0128a2e407e95dc3dee173875
pehara <pehara002@gmail.com>
2023-10-13 16:05:07 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,10 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// Step 1
+
+// Adding a button with a fun emoji
+const button = document.createElement("button");
+button.innerHTML = "( ˘▽˘)っ❤️";
+app.append(button);
