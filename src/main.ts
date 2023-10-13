da4c4b45bc309108107da29e55ab736dcca1d5df
pehara <pehara002@gmail.com>
2023-10-12 18:40:06 -0700

    changed the name of the game

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Pehara's game";
+const gameName = "Pehara's fun game";
 
 document.title = gameName;
 
